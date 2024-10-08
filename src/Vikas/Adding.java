8fc8d03a3a8446b67bfadbcbea26b7a8c2024157
vikas.lokesh <vikas.lokesh@STS-2917.sts.co.in>
2024-10-08 15:41:04 +0530

    first commit

---
+++ src/Vikas/Adding.java
@@ -0,0 +1,19 @@
+package Vikas;
+
+import java.util.Scanner;
+
+public class Adding {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the first number");
+		int n1=sc.nextInt();
+		System.out.println("Enter the second number");
+		int n2=sc.nextInt();
+		int n3=n1+n2;
+		System.out.println("Sum of n1 and n2 = "+ n3);
+		
+
+	}
+
+}
